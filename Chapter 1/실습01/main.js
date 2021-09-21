eb713b9dedb4e376fc3dc360138ff83ebb81b26a
Yena-Yun <cheryleduart@gmail.com>
2021-09-21 16:58:10 +0900

    실습 01 좌표007 성능개선

--- Chapter 1/실습01/main.js
+++ Chapter 1/실습01/main.js
@@ -2,6 +2,9 @@ let vertical = document.querySelector('.vertical');
 let horizontal = document.querySelector('.horizontal');
 let target = document.querySelector('.target');
 let tag = document.querySelector('.tag');
+let targetRect = target.getBoundingClientRect();
+let targetHalfWidth = targetRect.width / 2; // JS에서 계산할 때는 calc 필요없음 (CSS에서만 사용)
+let targetHalfHeight = targetRect.height / 2;
 
 document.addEventListener('mousemove', coordinates);
 
@@ -10,16 +13,12 @@ function coordinates(e) {
   // clientX,Y: 화면상에서의 마우스 위치
   let x = e.clientX;
   let y = e.clientY;
-  console.log(`${x} ${y}`);
 
-  vertical.style.left = `${x}px`;
-  horizontal.style.top = `${y}px`;
+  vertical.style.transform = `translateX(${x}px)`;
+  horizontal.style.transform = `translateY(${y}px`;
 
-  target.style.left = `${x}px`;
-  target.style.top = `${y}px`;
-
+  target.style.transform = `translate(${x - targetHalfWidth}px, ${y - targetHalfHeight}px)`;
+  tag.style.transform = `translate(${x}px, ${y}px)`;
   // tag div의 사이에 들어가는 텍스트로 마우스의 x, y 좌표
   tag.innerHTML = `${x}px, ${y}px`;
-  tag.style.left = `${x}px`;
-  tag.style.top = `${y}px`;
 }
