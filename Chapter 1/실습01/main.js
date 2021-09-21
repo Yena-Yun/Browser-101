87e9039aae886a9440d9fd318a8a016ac7763a38
Yena-Yun <cheryleduart@gmail.com>
2021-09-21 09:58:09 +0900

    실습01 복습

--- Chapter 1/실습01/main.js
+++ Chapter 1/실습01/main.js
@@ -3,9 +3,11 @@ let horizontal = document.querySelector('.horizontal');
 let target = document.querySelector('.target');
 let tag = document.querySelector('.tag');
 
-document.addEventListener('mousemove', resultFun);
+document.addEventListener('mousemove', coordinates);
 
-function resultFun(e) {
+// 여기서 e는 mousemove
+function coordinates(e) {
+  // clientX,Y: 화면상에서의 마우스 위치
   let x = e.clientX;
   let y = e.clientY;
   console.log(`${x} ${y}`);
@@ -16,7 +18,8 @@ function resultFun(e) {
   target.style.left = `${x}px`;
   target.style.top = `${y}px`;
 
+  // tag div의 사이에 들어가는 텍스트로 마우스의 x, y 좌표
   tag.innerHTML = `${x}px, ${y}px`;
-  tag.style.left = `${x + document.body.scrollLeft + 5}px`;
-  tag.style.top = `${y + document.body.scrollTop + 5}px`;
+  tag.style.left = `${x}px`;
+  tag.style.top = `${y}px`;
 }
